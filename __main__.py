26c834e076e11557c49d68fdac56a9ec19c9c0a1
TenmaMune <tenmachinen@gmail.com>
2022-12-06 16:08:05 +0100

    first commit

---
+++ __main__.py
@@ -0,0 +1,69 @@
+import shutil, json,os,sys
+from pathlib import Path
+import webbrowser
+
+l_args = sys.argv[1:]
+d_params = {'scope':'global', 'pkg_mgr':'pnpm'}
+d_options = {'scope':['global','local'], 'pkg_mgr':['npm','pnpm']}
+
+for arg in l_args:
+    if arg.__contains__('--') and arg.__contains__('='):
+        arg = arg.strip('--')
+        key, value = arg.split('=')
+        if key in d_params and value in d_options[key]:
+            d_params[key] = value
+            continue
+
+    print(f'\n    Arg : {arg} is not valid\n\n    Params must be:')
+    for key in d_params.keys():
+        values = ' or '.join(d_options[key])
+        print(f'        --{key}={values}')
+    print()
+    sys.exit()
+
+SRC_DIR = Path(__file__).resolve().parent / 'files'
+DST_DIR = Path(os.getcwd()).resolve()
+
+for file_name in os.listdir(SRC_DIR):
+    src_file_path = SRC_DIR / file_name
+    dst_file_path = DST_DIR / file_name
+    if os.path.isfile(src_file_path):
+        shutil.copy(src=src_file_path, dst=dst_file_path)
+    elif os.path.isdir(src_file_path):
+        shutil.copytree(src=src_file_path, dst=dst_file_path)
+
+pkg_mgr = d_params['pkg_mgr']
+
+if pkg_mgr == 'npm':
+    os.system('npm init y')
+else:
+    os.system('pnpm init')
+
+with open('package.json','r+') as file:
+    data = json.load(file)
+    data['scripts'] = {
+        'start' : 'webpack-dev-server --mode=development',
+        'build' : 'webpack --mode=production'
+        }
+    file.seek(0)
+    json.dump(data, file, indent=4)
+
+# install sass for sass-loader
+
+str_react_libs = 'react react-dom react-router-dom'
+str_babel_libs = '@babel/core @babel/preset-env @babel/preset-react'
+str_webpack_libs = 'webpack webpack-cli webpack-dev-server html-webpack-plugin'
+str_loaders_libs = 'babel-loader css-loader style-loader sass-loader file-loader'
+
+if d_params['scope'] == 'local':
+    os.system(f'{pkg_mgr} i {str_react_libs} --S')
+    os.system(f'{pkg_mgr} i {str_babel_libs} {str_webpack_libs} {str_loaders_libs} --D')
+else:
+    if pkg_mgr == 'npm':
+        os.system(f'npm link {str_react_libs} {str_babel_libs} {str_webpack_libs} {str_loaders_libs}')
+    else:
+        os.system(f'pnpm link -g {str_react_libs} {str_babel_libs} {str_webpack_libs} {str_loaders_libs}')
+
+
+webbrowser.open('http://localhost:8080/')
+os.system(f'{pkg_mgr} run start')
---
+++ files/webpack.config.js
@@ -0,0 +1,49 @@
+const HtmlWebPackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+    // publicPath: '/', // U K N O WN
+  },
+  // U K N O WN
+  // resolve: {
+  //   modules: [path.join(__dirname, 'src'), 'node_modules'],
+  //   alias: {
+  //     react: path.join(__dirname, 'node_modules', 'react'),
+  //   },
+  // },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: 'babel-loader',
+      },
+      {
+        test: /\.s?css$/i,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|gif|svg)$/i,
+        loader: 'file-loader',
+      },
+    ],
+  },
+  // Needed to use JSX file extension
+  resolve : {
+    extensions : ['.js', '.jsx']
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './src/index.html',
+      filename: './index.html'
+    }),
+  ],
+  // Needed to use BrowseRouter from react-router-dom
+  devServer: {
+    historyApiFallback: true,
+  }
+};
